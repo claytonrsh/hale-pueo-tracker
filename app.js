15c59426f028cafdb32cb477d7ffe8086be5b7f5
claytonrsh <7355948+claytonrsh@users.noreply.github.com>
2025-06-11 20:49:40 -1000

    try again to fix CORS with google app script redeploy...

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // --- Configuration ---
 const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTXl17fkaqYmdxU7pJ00Z4WWkglWndAJFhyKDz-axqR2u4zeUQOfgFR94eqp5ZipCwGZ3Ycm1eBu7Tl/pub?gid=0&single=true&output=csv';
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxhcLdP98QBGqHXlJO5otGEeV9_uISMR3DH3ddio0vTjlwnZbqnaKAkcpwfQRoW0MCk/exec';
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxbajuvOx0cmRpo5KZKxS1Vck644YIlOZCDSizVJ5iTy77qrAVhuVkpoE-GoihKQ-78/exec';
 
 // --- Global Variables ---
 let farmData = {};
